3837ad5937001f6855b980565c0f4d40283bf6c5
sunnysai12345 <sunnysai12345@gmail.com>
2017-02-06 16:05:58 +0530

    Data Loaded

---
+++ scripts/train.py
@@ -0,0 +1,62 @@
+'''
+Deep Learning Programming Assignment 1
+--------------------------------------
+Name:
+Roll No.:
+
+======================================
+Complete the functions in this file.
+Note: Do not change the function signatures of the train
+and test functions
+'''
+import numpy as np
+def softmax(x):
+    return np.exp(x)/(np.sum(np.exp(x),axis=1,keepdims=True))
+def cross_entropy(target,predicted):
+        #print(np.log(y1))
+        return -np.sum(target*np.log(predicted))/60000
+def train(trainX, trainY):
+    tx=trainX.reshape(60000,784)
+    w=np.zeros((784,10))
+    b=np.zeros((1,10))
+    y=np.dot(tx,w)+b
+    ty=np.zeros((60000,10))
+    ty[np.arange(60000),trainY] = 1
+    #ty=ty.reshape((60000,1,10))
+    #z=softmax(y)
+    #error=cross_entropy(ty,z)
+    for i in range(1000):
+        #print tx.shape,w.shape
+        y=np.dot(tx,w)+b
+        print(y[0])
+        soft=softmax(y)
+        loss=cross_entropy(ty,soft)
+        print ("In %d iteration loss is %d",i,loss)
+        eta=0.00001/60000
+        #print eta
+        w=w-eta*(tx.T.dot(soft-ty))
+        #print w[1:200]
+        b=b-np.around((np.sum(soft-ty)),4)
+        #print b
+    np.savetxt("weights.csv",w,delimiter=",")
+        
+
+def test(testX):
+    '''
+    Complete this function.
+    This function must read the weight files and
+    return the predicted labels.
+    The returned object must be a 1-dimensional numpy array of
+    length equal to the number of examples. The i-th element
+    of the array should contain the label of the i-th test
+    example.
+    '''
+    w=np.genfromtxt("weights.csv",delimiter=",")
+    print w.shape,w
+    newy=testX.reshape(10000,784).dot(w)
+    print newy.shape
+    newys=softmax(newy)
+    output=newys.argmax(axis=1)
+    #acc=np.sum(out==testY)
+    #print acc
+    return output
